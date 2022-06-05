47a68f07e46406ac606581dcdadb7c3c7128c511
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-06-05 08:56:04 +0530

    append(),removeAt(),removeFirstNode(),removelast

--- LinkedList/include/linkedlist.hpp
+++ LinkedList/include/linkedlist.hpp
@@ -1,7 +1,7 @@
 #ifndef __LINKED__LIST__
 #define __LINKED__LIST__
 #include <iostream>
-
+#include <iomanip>
 template <typename T>
 class LinkedList
 {
@@ -10,13 +10,13 @@ private:
     {
         T data;
         Node *next;
-        Node(const T& data)
+        Node(const T &data)
         {
-            this->data=data;
+            this->data = data;
         };
     };
 
-public:
+private:
     Node *head, *tail;
     size_t size;
 
@@ -24,12 +24,120 @@ public:
     LinkedList();
     void display(void) const;
     void append(const T &data);
+    void remove(const T &);
+    void prepend(const T &);
+    void removeFirstNode();
+    void removeLastNode();
+    void removeAt(const size_t &pos);
+    size_t getSize() const { return this->size; };
+};
+template <typename T>
+void LinkedList<T>::removeAt(const size_t &pos)
+{
+    if (pos > this->size || pos < 1)
+    {
+        return;
+    }
+    else if (pos == 1)
+    {
+        this->removeFirstNode();
+    }
+    else if (pos == this->size)
+    {
+        this->removeLastNode();
+    }
+    else if (this->head != nullptr)
+    {
+        Node *prev{head}, *cur{head};
+        for (size_t i = 0; i < pos; ++i)
+        {
+            prev = cur;
+            cur = cur->next;
+        };
+        prev->next = cur->next;
+        free(cur);
+        --this->size;
+    };
+}
+template <typename T>
+void LinkedList<T>::removeLastNode()
+{
+    Node *prev{head}, *cur{head};
+    if (head->next == nullptr)
+    {
+        this->removeFirstNode();
+    }
+    else if (head != nullptr)
+    {
+        while (cur->next != nullptr)
+        {
+            prev = cur;
+            cur = cur->next;
+        };
+        tail = prev;
+        prev->next = nullptr;
+        free(cur);
+        --this->size;
+    };
+}
+template <typename T>
+void LinkedList<T>::removeFirstNode()
+{
+    if (head != nullptr)
+    {
+
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        --this->size;
+    };
 };
 
+template <typename T>
+void LinkedList<T>::prepend(const T &data)
+{
+    Node *node = new Node(data);
+
+    if (this->head == nullptr)
+    {
+        head = node;
+        tail = node;
+    }
+    else
+    {
+
+        node->next = head->next;
+        head->next = node;
+    };
+}
+
+template <typename T>
+void LinkedList<T>::remove(const T &key)
+{
+    Node *temp_head = head;
+    Node *temp_node = this->head;
+    Node *step_back = nullptr;
+    while (temp_node)
+    {
+        if (temp_node->data == key)
+        {
+            step_back = temp_node->next;
+            free(temp_node);
+            --this->size;
+            return;
+        }
+        else
+        {
+            step_back = temp_node;
+            temp_node = temp_node->next;
+        };
+    }
+}
 template <typename T>
 void LinkedList<T>::append(const T &data)
 {
     Node *temp_node = new Node(data);
+    // temp_node->next=nullptr;
 
     if (head == nullptr)
     {
@@ -39,7 +147,7 @@ void LinkedList<T>::append(const T &data)
     else
     {
         tail->next = temp_node;
-        tail=temp_node;
+        tail = temp_node;
     }
     ++size;
 }
@@ -56,12 +164,12 @@ template <typename T>
 void LinkedList<T>::display(void) const
 {
     printf("sizeof obj type : %lu byte\n", sizeof(T));
-    Node* temp_node=head;
-    for(size_t i=0;i<size;++i)
+    const Node *temp_node = this->head;
+    while (temp_node)
     {
-        printf("Node[%lu]->",i);
-        std::cout<<temp_node->data<<"\n";
-        temp_node=temp_node->next;
+
+        std::cout << temp_node->data << "\n";
+        temp_node = temp_node->next;
     };
 };
 
--- LinkedList/src/main.cxx
+++ LinkedList/src/main.cxx
@@ -1,14 +1,41 @@
-#include"../include/linkedlist.hpp"
+#include "../include/linkedlist.hpp"
 int main(void)
 {
-    LinkedList<double> *rates;
-    // rates.display();
+    LinkedList<double> *rates{};
+    rates = (LinkedList<double> *)malloc(sizeof(LinkedList<double>));
+    LinkedList<char> *Alpha = (LinkedList<char> *)malloc(sizeof(LinkedList<char>));
+    for (char alpha = 'a'; alpha <= 'g'; ++alpha)
+    {
+        Alpha->append(alpha);
+    };
+    printf("size of Alpha : %lu\n", Alpha->getSize());
+    Alpha->display();
+    // Alpha->remove('c');
+    // Alpha->prepend('z');
+    Alpha->removeFirstNode();
+    Alpha->removeLastNode();
+    Alpha->removeLastNode();
+    Alpha->removeAt(1);
+    printf("\n----------------------------------\n");
+    Alpha->display();
+    // for (short i = 0; i < 10; ++i)
+    // {
+    //     rates->append((double)(i * 0.04));
+    // }
+    // if (rates)
+    // {
 
-for(auto i=0;i<12000;++i)
-{
-    rates->append(i*0.04);
-}
-    rates->display();
-
+    //     rates->display();
+    //     printf("\nsize of rates list: %lu\nafter removing 3 ...\n", rates->getSize());
+    //     rates->remove(0.2);
+    //     rates->display();
+    //     printf("\nsize of rates list: %lu\n",rates->getSize());
+    // }
+    // else
+    // {
+    //     printf("rates is nullptr!\n");
+    // };
+    free(Alpha);
+    free(rates);
     return 0;
 };
